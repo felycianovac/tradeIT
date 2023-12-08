6d439bcfa968533438e1b3013dc747dc72ceeb6a
felycianovac <felicianovac4@gmail.com>
2023-12-08 23:09:38 +0200

    user repo

--- src/main/java/org/example/user/UserRepository.java
+++ src/main/java/org/example/user/UserRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
     Optional<User> findByUsername(String username);
+
+
     Optional<User> findByEmail(String email);
-
-
 }
