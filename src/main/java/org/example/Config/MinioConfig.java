f812ab8e7e36877849560d3b7193bd611bb3e1bd
felycianovac <felicianovac4@gmail.com>
2023-12-08 23:07:22 +0200

    deployment reconfig

--- src/main/java/org/example/Config/MinioConfig.java
+++ src/main/java/org/example/Config/MinioConfig.java
@@ -1,6 +1,8 @@
 package org.example.Config;
 
 import io.minio.MinioClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
